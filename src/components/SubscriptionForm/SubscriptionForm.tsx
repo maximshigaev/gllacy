2be8789473f6169ddceea31e4442316f9c69bac5
Maxim <financial_monster@mail.ru>
2022-04-19 18:15:43 +0400

    add basic styles for the SubscriptionForm component

--- src/components/SubscriptionForm/SubscriptionForm.tsx
+++ src/components/SubscriptionForm/SubscriptionForm.tsx
@@ -1,15 +1,28 @@
 import { FC } from 'react';
 
+// Styles
+import './subscriptionForm.scss'; 
+
 export const SubscriptionForm: FC = () => (
   <section>
     <h2 className="visually-hidden">Форма подписки</h2>
-    <p>
+    <p className="subscription-form__text">
       Подпишитесь на нашу сладкую рассылку и будете всегда в курсе всего самого вкусного, что у нас происходит. Обещаем
       не спамить и не слать всякой ненужной ерунды. Честно =) 
     </p>
     <form>
-      <input type="email" placeholder="Электронная почта" />
-      <button type="button" title="Отправить">Отправить</button>
+      <input
+        className="subscription-form__input"
+        type="email"
+        placeholder="Электронная почта"
+      />
+      <button
+        className="button"
+        type="button"
+        title="Отправить"
+      >
+        Отправить
+      </button>
     </form>
   </section>
 );
