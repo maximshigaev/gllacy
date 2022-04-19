cfce04b77de783c20b0225132c84114667b46a8d
Maxim <financial_monster@mail.ru>
2022-04-19 18:03:06 +0400

    add basic styles for the News component

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react';
 
+// Styles
+import './News.scss';
+
 export const News: FC = () => (
   <article>
-    <h2>Новое в нашем блоге</h2>
-    <h3>
+    <h2 className="news__title">Новое в нашем блоге</h2>
+    <h3 className="news__heading">
       10 способов сервировки фруктовых щербетов к столу
     </h3>
   </article>
