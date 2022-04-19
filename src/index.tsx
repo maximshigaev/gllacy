f8bfd87de8c963898982840801c2c637f5698658
Maxim <financial_monster@mail.ru>
2022-04-19 13:53:23 +0400

    add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Components
 import { App } from './components';
+
+// Styles
+import './styles/reset.scss';
+import './styles/scaffolding.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
