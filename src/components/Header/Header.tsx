7c6736c2a5b2353a7d0e081d8659a83612cd9026
Maxim <financial_monster@mail.ru>
2022-04-19 15:07:14 +0400

    add basic styles for the Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 
+// Styles
+import './header.scss';
+
 // Images
 import logo from '../../images/logo.png';
 
@@ -16,27 +19,27 @@ export const Header: FC = () => (
       </a>
       <ul>
         <li>
-          <a href="#">Каталог</a>
+          <a className="header__nav-link" href="#">Каталог</a>
         </li>
         <li>
-          <a href="#">Доставка и оплата</a>
+          <a className="header__nav-link" href="#">Доставка и оплата</a>
         </li>
         <li>
-          <a href="#">О Компании</a>
+          <a className="header__nav-link" href="#">О Компании</a>
         </li>
       </ul>
       <button type="button" />
       <ul>
         <li>
-          <a href="#">Вход</a>
+          <a className="header__login-link" href="#">Вход</a>
         </li>
         <li>
-          <a href="#">Пусто</a>
+          <a className="header__cart- link" href="#">Пусто</a>
         </li>
       </ul>
       <div>
-        <p>С 10 до 20, ежедневно</p>
-        <p>8 812 450-25-25</p>
+        <p className="header__time">С 10 до 20, ежедневно</p>
+        <p className="header__phone">8 812 450-25-25</p>
       </div>
     </nav>
   </header>
