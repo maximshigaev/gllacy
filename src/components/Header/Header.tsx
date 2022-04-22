0abf6d255cfcc0a06d3005fad7f30fe68c472655
Maxim <financial_monster@mail.ru>
2022-04-22 14:38:47 +0400

    add basic grid for the Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,16 +8,17 @@ import logo from '../../images/logo.png';
 
 export const Header: FC = () => (
   <header>
-    <nav>
+    <nav className="header__nav">
       <a href="#">
         <img
+          className="header__logo"
           src={logo}
           alt="Логотип Gllacy"
           width="154"
           height="62"
         />
       </a>
-      <ul>
+      <ul className="header__nav-links">
         <li>
           <a className="header__nav-link" href="#">Каталог</a>
         </li>
@@ -28,16 +29,16 @@ export const Header: FC = () => (
           <a className="header__nav-link" href="#">О Компании</a>
         </li>
       </ul>
-      <button type="button" />
-      <ul>
-        <li>
-          <a className="header__login-link" href="#">Вход</a>
-        </li>
-        <li>
-          <a className="header__cart- link" href="#">Пусто</a>
-        </li>
-      </ul>
-      <div>
+      <div className="header__cart-container">
+        <button type="button" />
+        <ul>
+          <li>
+            <a className="header__login-link" href="#">Вход</a>
+          </li>
+          <li>
+            <a className="header__cart-link" href="#">Пусто</a>
+          </li>
+        </ul>
         <p className="header__time">С 10 до 20, ежедневно</p>
         <p className="header__phone">8 812 450-25-25</p>
       </div>
