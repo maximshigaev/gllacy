c2beb4cce2f184c9624751b3fb8a3b3d9a800e7e
Maxim <financial_monster@mail.ru>
2022-04-22 14:58:53 +0400

    add basic grid for the Promotions component

--- src/components/Promotions/Promotions.tsx
+++ src/components/Promotions/Promotions.tsx
@@ -3,13 +3,16 @@ import { FC } from 'react';
 // Components
 import { PromotionItem } from '../';
 
+// Styles
+import './promotions.scss';
+
 // Constants
 import { promotionItems } from '../../constants';
 
 export const Promotions: FC = () => (
   <section>
     <h2 className="visually-hidden">Акции</h2>
-    <ul>
+    <ul className="promotions__list">
       {promotionItems.map(({ id, text, title, buttonText }) => (
         <li key={id}>
           <PromotionItem
