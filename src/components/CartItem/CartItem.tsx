41f35c050f590bc9d553c1baa7062b81c6694c2b
Maxim <financial_monster@mail.ru>
2022-04-29 13:11:18 +0400

    add layout and styles for the CartItem component

---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,43 @@
+import {
+  FC,
+  useCallback,
+} from 'react';
+
+// Styles
+import './cartItem.scss';
+
+// Types
+import { TCartItem } from '../../types';
+
+type TProps = {
+  cartItem: TCartItem;
+  deleteCartItem: (id: number) => void;
+}
+
+export const CartItem: FC<TProps> = ({ cartItem: { src, name, weight, price, id }, deleteCartItem }) => {
+  const handleDeleteBtnClick = useCallback(
+    () => deleteCartItem(id),
+    [deleteCartItem, id],
+  );
+
+  return (
+    <div className="cart-item">
+      <button
+        className="cart-item__btn"
+        title="Удалить"
+        onClick={handleDeleteBtnClick}
+      />
+      <img
+        className="cart-item__icon"
+        src={src}
+        alt="Мини-фото мороженого"
+      />
+      <p className="cart-item__name">{name}</p>
+      <div className="cart-item__weight">
+        {weight} кг x 
+        <span className="cart-item__price"> {price} руб.</span>
+      </div>
+      <span className="cart-item__cost">{price * weight} руб.</span>
+    </div>
+  );
+}
