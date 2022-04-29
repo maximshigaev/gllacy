3d13f2e075cee852289f322ae5f9cf5fd4452bba
Maxim <financial_monster@mail.ru>
2022-04-29 16:33:52 +0400

    add styles for the Promotions and PromotionItems components

--- src/components/PromotionItem/PromotionItem.tsx
+++ src/components/PromotionItem/PromotionItem.tsx
@@ -7,18 +7,19 @@ type TProps = {
   title: string;
   text: string;
   buttonText: string;
+  id: number;
 } 
 
-export const PromotionItem: FC<TProps> = ({ title, text, buttonText }) => (
-  <>
-    <h3 className="promotionItem__title">{title}</h3>
-    <p className="promotionItem__text">{text}</p>
+export const PromotionItem: FC<TProps> = ({ title, text, buttonText, id }) => (
+  <div className={`promotion-item promotion-item--${id}`}>
+    <h3 className="promotion-item__title">{title}</h3>
+    <p className="promotion-item__text">{text}</p>
     <button
-      className="button"
+      className="button promotion-item__btn"
       type="button"
       title="Хочу варенье!"
     >
       {buttonText}
     </button>
-  </>
+  </div>
 );
--- src/components/Promotions/Promotions.tsx
+++ src/components/Promotions/Promotions.tsx
@@ -19,6 +19,7 @@ export const Promotions: FC = () => (
             title={title}
             text={text}
             buttonText={buttonText}
+            id={id}
           />
         </li>  
       ))}
