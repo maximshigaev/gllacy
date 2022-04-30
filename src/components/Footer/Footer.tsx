974df99856c558cabd18b2d4b1602811fa8157d9
Maxim <financial_monster@mail.ru>
2022-04-30 20:42:22 +0400

    add layout and styles for the Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,47 +4,99 @@ import { FC } from 'react';
 import './footer.scss';
 
 export const Footer: FC = () => (
-  <footer>
+  <footer className="footer">
     <h2 className="visually-hidden">Полезные ссылки</h2>
-    <ul>
-      <li>
-        <a href="#">
+    <ul className="footer__social-list">
+      <li className="footer__social-item">
+        <a
+          className="footer__social-link footer__social-link--twitter"
+          title="Твиттер"
+          href="#"
+        >
           <span className="visually-hidden">Твиттер</span>
         </a>
       </li>
-      <li>
-        <a href="#">
+      <li className="footer__social-item">
+        <a
+          className="footer__social-link footer__social-link--instagram"
+          title="Инстаграм"
+          href="#"
+        >
           <span className="visually-hidden">Инстаграм</span>
         </a>
       </li>
-      <li>
-        <a href="#">
+      <li className="footer__social-item">
+        <a
+          className="footer__social-link footer__social-link--facebook"
+          title="Фейсбук"
+          href="#"
+        >
           <span className="visually-hidden">Фейсбук</span>
         </a>
       </li>
-      <li>
-        <a href="#">
+      <li className="footer__social-item">
+        <a
+          className="footer__social-link footer__social-link--vk"
+          title="Вконтакте"
+          href="#"
+        >
           <span className="visually-hidden">Вконтакте</span>
         </a>
       </li>
     </ul>
-    <ul>
-      <li>
-        <a className="footer__info-link footer__info-link--suppliers" href="#">Для поставщиков</a>
+    <ul className="footer__info-links">
+      <li className="footer__info-item">
+        <a
+          className="footer__info-link footer__info-link--suppliers"
+          title="Для поставщиков"
+          href="#"
+        >
+          Для поставщиков
+        </a>
       </li>
-      <li>
-        <a className="footer__info-link" href="#">Наши документы</a>
+      <li className="footer__info-item">
+        <a
+          className="footer__info-link"
+          title="Наши документы"
+          href="#"
+        >
+          Наши документы
+        </a>
       </li>
-      <li>
-        <a className="footer__info-link" href="#">О производстве</a>
+      <li className="footer__info-item">
+        <a
+          className="footer__info-link"
+          title="О производстве"
+          href="#"
+        >
+          О производстве
+        </a>
       </li>
-      <li>
-        <a className="footer__info-link" href="#">Экологические стандарты</a>
+      <li className="footer__info-item">
+        <a
+          className="footer__info-link"
+          title="Экологические стандарты"
+          href="#"
+        >
+          Экологические стандарты
+        </a>
       </li>
     </ul>
     <div className="footer__copyright">
-      <p></p>
-      Сделано в <a className="footer__copyright-link" href="#">HTML Academy</a> © 2017
+      <a
+        className="footer__copyright-logo"
+        title="HTML Academy"
+        href="#"
+      />
+        Сделано в
+      <a
+        className="footer__copyright-link"
+        title="HTML Academy"
+        href="#"
+      >
+        {` HTML Academy`}
+      </a>
+        {` © 2017`}
     </div>
   </footer>
 );
